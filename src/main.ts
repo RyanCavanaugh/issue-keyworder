6b570c558e78008c6b82e1ba9c55e74e83d835ec
Ryan Cavanaugh <RyanCavanaugh@users.noreply.github.com>
2019-12-02 12:51:49 -0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,7 @@ async function run() {
         const issue = await gh.issues.get({ issue_number: github.context.issue.number, owner: github.context.issue.owner, repo: github.context.issue.repo });
         if (issue.data.body.indexOf("fish") >= 0) {
             await gh.issues.createComment({
-                body: "You said the magic word",
+                body: "You said the magic word: " + core.getInput("some_secret"),
                 issue_number: github.context.issue.number, owner: github.context.issue.owner, repo: github.context.issue.repo
             });
         }
